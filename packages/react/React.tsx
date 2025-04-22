021832379bf0ecbe2f09ae38626166a07864f3c7
vv3rd <lyudskoy@gmail.com>
2025-04-22 22:59:44 +0500

    added useTracedSelector

--- packages/react/React.tsx
+++ packages/react/React.tsx
@@ -1,4 +1,12 @@
-import React, { createContext, use, useCallback, useSyncExternalStore } from "react";
+import React, {
+    createContext,
+    use,
+    useCallback,
+    useDebugValue,
+    useMemo,
+    useRef,
+    useSyncExternalStore,
+} from "react";
 import { Control, Msg } from "../core/definition";
 import { bindMsgFactories, MsgFactory } from "../core/messages";
 import { Wire } from "../core/wiring";
@@ -43,4 +51,61 @@ export function useDispatch<T extends { [key: string]: MsgFactory<Msg<any>, any[
     } else {
         return ctl.dispatch;
     }
+}
+
+export function useTracedSelector<
+    TArgs extends any[],
+    TState extends object = {},
+    TSelected = unknown,
+>(selector: (state: TState, ...args: TArgs) => TSelected, ...args: TArgs): TSelected {
+    const { current: before } = useRef({ selector });
+    if (before.selector !== selector) {
+        before.selector = selector;
+        console.warn(
+            "useTracedSelector expects selector arg to not change, " +
+                "otherwise the hook is redundant",
+        );
+    }
+
+    const deps = [selector, ...args];
+    const memoized = useMemo(() => {
+        let cache: null | {
+            value: TSelected;
+            state: TState;
+            keys: Set<keyof TState>;
+        } = null;
+
+        return (state: TState) => {
+            whenHasCache: if (cache) {
+                for (const key of cache.keys) {
+                    if (state[key] !== cache.state[key]) {
+                        break whenHasCache;
+                    }
+                }
+                return cache.value;
+            }
+
+            const keys = new Set<keyof TState>();
+            const tracedState = new Proxy(state, {
+                get(_, p) {
+                    const key = p as keyof TState;
+                    const val = state[key];
+                    if (keys.has(key) === false) {
+                        keys.add(key);
+                    }
+                    return val;
+                },
+            });
+
+            const value = selector(tracedState, ...args);
+            {
+                cache = { state, value, keys };
+            }
+            return value;
+        };
+    }, deps);
+
+    const value = useSelector(memoized);
+    useDebugValue(value);
+    return value;
 }
